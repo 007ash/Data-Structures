97594aee72b3ac45b7ad2c871b8bbd7e0a7fb9c3
007ash <6ashwanth007@gmail.com>
2025-07-10 20:26:04 +0530

    Linked list

---
+++ Arrays/likedlist.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.next = None
+        self.left = None
+        self.right = None
+
+class LinkedList:
+    pass
+
