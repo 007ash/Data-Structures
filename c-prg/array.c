d048512e6e189590fa0e8c39bb11991e5fc0c548
007ash <6ashwanth007@gmail.com>
2025-07-18 21:49:28 +0530

    c

--- c-prg/array.c
+++ c-prg/array.c
@@ -4,5 +4,15 @@ int main(){
     for(i=0; i<=5; i++){
         printf("%d\n", a);
     }
+    display(2,4);
     return 0;
 }
+
+int display(int a, int p){
+    int i;
+    for(i=0; i<=p; i++){
+        printf("%d ",a);
+    }
+}
+
+
