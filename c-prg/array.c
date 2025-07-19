fd646fe91f102477363a129bd84542fbd5dbb8fe
007ash <6ashwanth007@gmail.com>
2025-07-19 22:34:54 +0530

    arithmetic123

--- c-prg/array.c
+++ c-prg/array.c
@@ -1,26 +1,21 @@
 int add(int a, int b){
     return a+b;
 }
-
 int sub(int a, int b){
     return a-b;
 }
-
 int mul(int a, int b){
     return a*b;
 }
-
 int div(int a, int b){
     return a/b;
 }
-
 int main(){
     int x = 5, y = 10;
     printf("%d\n",add(x,y));
     printf("%d\n",sub(x,y));
     printf("%d\n", mul(x,y));
     printf("%d\n", div(x,y));
-
     printf("%d\n", add(x,y)+sub(x,y));
     return 0;
 }
